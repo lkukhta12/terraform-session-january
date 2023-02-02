65e6cd497921dbe83c051c1e0fb8b1375563fe7a
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-01 22:53:01 -0600

    updated readme

--- session-2/ec2.tf
+++ session-2/ec2.tf
@@ -3,31 +3,41 @@ resource "aws_instance" "first_ec2" {
     instance_type = "t2.micro"
 }
 
-// A block is a container for other conten: 
-// A block has a type (resource in this example) 
+// Blocks: 
+// A block is a container for other content: 
+
+// A block has a type: resource in this example 
 // Each block type defines how many labels must follow the type keyword 
-// The resource block type expects two labels, which are “was_instance" 
+// The resource block type expects two labels, which are aws_instance and first_ec2 in the example above.
+
 // Terraform has 2 main Blocks : 
-// Resource Block - Create and Manage resources 
-// Resource Block expects 2 labels which are "First label" and "Second label" 
-//   1.First Label: 
-//       - This is predefined by Terraform 
-//       - This label indicates the resource 
-//       Example: “was_instance” 
-//   2. Second Label 
-//       - This is defined by Author 
-//       - This label indicates the logical name of the resource. Logical name or id must be unique 
-// Data Source Block - Fetch the data from existing resources 
-// ARGUMENTS: 
+// 1.Resource Block: Create and Manage resources 
+// 2. Data Source Block: Fetch the data from existing resources.
+
+// Resouces block expects 2 labels:  
+// 1.First Label: 
+//   --This is predefined by Terraform 
+//   --This label indicates the resource 
+//   Example: “aws_instance” 
+// 2.Second Label 
+// --This is defined by Author 
+// -- This label indicates the logical name of the resource. Logical name or id must be unique 
+// Data Source Block - Fetch the data from existing resources. 
+
+// Arguments: 
 // An argument assigns a value to a particular name 
-// The identifier before he equals sign is the argument name, and the expression after the equals sign is the argument’s value 
-// Argument consist of name and value 
+// The identifier before the equal sign is the argument name, and the expression after the equal sign is the argument’s value.
+
+// Argument consist of Name and Value 
 // Argument Name: 
-//  - This is predefined by Terraform 
-//  - Configuration of your resource 
-//  - Example: ami 
+// --This is predefined by Terraform 
+// --Configuration of your resource 
+// --Example: ami 
+
 // Argument Value: 
-//   - This is predefined by Author 
-// Working directory: 
-// - Where you have all your working configuration file  
-// You run terraform command on your working directory 
+// --This is predefined by Author 
+// --Value fo the configurations
+// --Example: "ami-0aa7d40eeae50c9a9"
+//    Working directory: 
+//       - Where you have all Terraform configuration file  
+//       - example: session-2
