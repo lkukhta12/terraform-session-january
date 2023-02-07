2b3fa1952ac358707c4c708d6327b5f438158aba
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-07 03:12:57 +0000

    added vars file

--- session-3/ec.tf
+++ session-3/ec.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "Session-3EC2" {
-    ami = var.instance_type
+    ami = var.ami
     instance_type = var.instance_type            # insted of specifing the t2.micro we refer to variables where it will look for the instance type
     vpc_security_group_ids = [ aws_security_group.main.id ] # since id for sg is NOT known we do not indicate id
     tags = {                   # map
