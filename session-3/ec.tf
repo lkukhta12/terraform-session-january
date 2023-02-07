3e02c9606c53c1d17e66628d32e738ae2bbb2762
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-07 02:04:14 +0000

    added sg to attach to ec2

--- session-3/ec.tf
+++ session-3/ec.tf
@@ -1,4 +1,14 @@
 resource "aws_instance" "Session-3EC2" {
     ami = "ami-0aa7d40eeae50c9a9"
     instance_type = "t2.micro"
+    vpc_security_group_ids = ["aws_security_group.main.id"] # since id for sg is NOT known we do not indicate id
 }
+
+#List
+
+#List of strings = ["fruits", "veggies", "dairy"]
+#List of numbers = [""""""]
+
+#Reference to Resource is used to add securioty group to the instance
+#Attiribute known after creation
+#Reference before creating 
