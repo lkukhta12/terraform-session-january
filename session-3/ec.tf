0271dafe73c6bbb64f1f18be8e837d507be9b535
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-07 02:14:56 +0000

    added sg to attach to ec2

--- session-3/ec.tf
+++ session-3/ec.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Session-3EC2" {
     ami = "ami-0aa7d40eeae50c9a9"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["aws_security_group.main.id"] # since id for sg is NOT known we do not indicate id
+    vpc_security_group_ids = [ aws_security_group.main.id ] # since id for sg is NOT known we do not indicate id
 }
 
 #List
@@ -12,3 +12,7 @@ resource "aws_instance" "Session-3EC2" {
 #Reference to Resource is used to add securioty group to the instance
 #Attiribute known after creation
 #Reference before creating 
+
+
+#"aws_security_group.main.id" = static or hard coded value string, here 
+# aws_security_group.main.id = reference - this is dymanic , so here you can use for terraform to go and look for id
