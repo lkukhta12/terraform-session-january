6d0ec79b4912c15f05cb24bfeccbb919d6a8295c
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-07 03:41:59 +0000

    added changes to output file

--- session-3/outputs.tf
+++ session-3/outputs.tf
@@ -4,7 +4,7 @@ output "instance_ip_address" {
     value       = aws_instance.Session-3EC2.public_ip
     description = "The public IP address of the main EC2 instance."
 }
-output "instance_volume_id" {
-    value       = aws_instance.Session-3EC2.instance_volume_id
-    description = "The volume if of the main EC2 instance."
+output "instance_state" {
+    value       = aws_instance.Session-3EC2.instance_state
+    description = "The state of the main EC2 instance."
 }
