d70a9468fdf122d270edeb6a9cc05e78bccd93aa
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-09 03:28:31 +0000

    added ec2 with format function

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "Session-3EC2" {
     instance_type = var.instance_type            # insted of specifing the t2.micro we refer to variables where it will look for the instance type
     vpc_security_group_ids = [ aws_security_group.main.id ] # since id for sg is NOT known we do not indicate id
     tags = {                   
-        Name = "${var.env}-instance"
+        Name = "${var.env}-instance" # dev-instance
+        Name = format ("%s-instance", var.env)   # dev-instance
     }
 }
 resource "aws_instance" "Session-3EC2_frontend" {
