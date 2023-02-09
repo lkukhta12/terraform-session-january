1d5508f84e67668ca49e747902aff778cc9ef9c9
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-09 01:41:40 +0000

    added data source

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "Session-3EC2" {
     ami = data.aws_ami.amazon_linux_2.id
     instance_type = var.instance_type            # insted of specifing the t2.micro we refer to variables where it will look for the instance type
-    vpc_security_group_ids = [ aws_security_group.main.sg.id ] # since id for sg is NOT known we do not indicate id
+    vpc_security_group_ids = [ aws_security_group.main.id ] # since id for sg is NOT known we do not indicate id
     tags = {                   # map
         Name = var.env
     }
