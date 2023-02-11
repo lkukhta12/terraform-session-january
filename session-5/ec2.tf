c0c4c5eeb7a1ec62489db07e0d139b672d11b648
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-11 18:24:03 +0000

    adding 3 ec2

--- session-5/ec2.tf
+++ session-5/ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "Session-3EC2" {
+    count =  3
     ami = data.aws_ami.amazon_linux_2.id
     instance_type = var.instance_type            # insted of specifing the t2.micro we refer to variables where it will look for the instance type
     vpc_security_group_ids = [ aws_security_group.main.id ] # since id for sg is NOT known we do not indicate id
@@ -8,9 +9,8 @@ resource "aws_instance" "Session-3EC2" {
     }
 }
 
-
+#count is meta argument
 # References:
-
 #Resources
 #Input variables
 #Local values
