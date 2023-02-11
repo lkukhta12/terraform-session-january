87e5bf48fd92f0709fdd80285dfec558ccc125d9
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-11 19:08:05 +0000

    opening more ports

--- session-5/sg.tf
+++ session-5/sg.tf
@@ -4,15 +4,14 @@ resource "aws_security_group" "main" {
     vpc_id      = "vpc-0e6848796d1f4ca2d" # my default aws vpc id or other vpc
 }
 resource "aws_security_group_rule" "ingress" {
-  count = 2
+  count = 2 # to open 2 ports from variables file
   type              = "ingress"
-  to_port           = element(var.ports, 0) # port 22
-  protocol          = "tcp"
-  from_port         = element(var.ports, 0) # port 22
+  to_port           = element( var.ports, count.index ) # port 22
+  from_port         = element( var.ports, count.index ) # port 22
   security_group_id = aws_security_group.main.id
+  protocol          = "tcp"
   cidr_blocks      = ["68.129.177.23/32"]
 }
-
 resource "aws_security_group_rule" "egress" {
   type              = "egress"
   to_port           = 0
@@ -21,5 +20,9 @@ resource "aws_security_group_rule" "egress" {
   security_group_id = aws_security_group.main.id
   cidr_blocks      = ["0.0.0.0/0"]
 }  
- 
 # element(var.port, 0)
+#count = 2
+#count.index = 0, 1
+
+#count = 5
+#count.index = 0, 2, 3, 4
