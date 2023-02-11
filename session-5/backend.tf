ece200fee78ace2d49c13a8aebcdb957e42ba741
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-11 17:35:35 +0000

    adding lock to session-5

--- session-5/backend.tf
+++ session-5/backend.tf
@@ -3,5 +3,10 @@ terraform {
     bucket = "terraform-session-september-backend-lididi"
     region = "us-east-1"
     key    = "session-5/terraform.tfstate"  # this is indication where does terraform is going to store my files
+    dynamodb_table = "terraform-session-sep-statelock"  # Lock table name
   }
 }
+
+
+# You can not use any variables
+# If yo change words in session-5 than it will create a new obeject in s3 - tryo not to tounch this
