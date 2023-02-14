89f8683f8604051a6c749df2d22241a593d744f6
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-14 01:11:34 +0000

    updated backend to craete qa

--- session-6/backend.tf
+++ session-6/backend.tf
@@ -2,11 +2,12 @@ terraform {
   backend "s3" {
     bucket = "terraform-session-september-backend-lididi"
     region = "us-east-1"
-    key    = "session-6/filestructure/dev/terraform.ftstate"  # this is indication where does terraform is going to store my files
+    key    = "session-6/filestructure/qa/terraform.ftstate"  # this is indication where does terraform is going to store my files
     dynamodb_table = "terraform-session-sep-statelock"  # Lock table name
   }
 }
-
-
 # You can not use any variables
 # If yo change words in session-6 than it will create a new obeject in s3 - tryo not to tounch this
+
+# if you want to isolate infrastructure and reuse your config files, you must isolate your backend first
+# Isolating backend (terraform.ftstate) = Isolating Enviroment
