f706eb7a764d39d354aafac476702ffcda503aae
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-14 01:34:02 +0000

    qa.tfvars and qa.tfvars added

--- session-6/backend.tf
+++ session-6/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-session-september-backend-lididi"
     region = "us-east-1"
-    key    = "session-6/filestructure/qa/terraform.ftstate"  # this is indication where does terraform is going to store my files
+    key    = "session-6/filestructure/dev/terraform.ftstate"  # this is indication where does terraform is going to store my files
     dynamodb_table = "terraform-session-sep-statelock"  # Lock table name
   }
 }
