fa85b7428ae669dc15466799660dd4340f5496b3
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-14 02:00:47 +0000

    added bash script

--- session-6/terraform.sh
+++ session-6/terraform.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+rm -rf .terraform*
+read -p "Which enviroent would you like" ENV 
+echo $ENV
+sed -i "s/_evn_$ENV/g" backend.tf
+
+echo "Enviroment is set to $ENV"
+
+terraform init
+terraform plan -var-file=$ENV.ftvars
+
+read -p "Are you sure that you would like to apply?" apply
+if $apply == [yes]:
+then terraform apply -var-file=$ENV.ftvars -auto-approve
+else echo "Terraform is not going to apply"
+fi
+
+echo "Rolling back the backend"
+sed -i "s/$ENV/_env_/g" backend.tf
+echo "Script run succesfully"
+
+##### terraform apply -var-file=$ENV.tfvars # user -auto-approve only if you sure your script has no mistakes
+
+#1. Updating backend file
+#2. Terraform init
+#3. Terraform plan -var-file=
