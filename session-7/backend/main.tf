8607ef304626be805dff805f85554842e19b2218
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-16 03:16:07 +0000

    added main

--- session-7/backend/main.tf
+++ session-7/backend/main.tf
@@ -1,116 +1,27 @@
-# providers-team-env-app-resource-index#
-# aws-devops-dev-tom-sqs-0
-variable "tags" {
-    type = map()
-    default = {
-        Enviroment = "dev"
-        Team =  "DevOps"
-        App = "Jerry"
-        Managed_by = "Terraform"
-        Owner = "Lidiya"
-        Department = "BBC"  
-    }
-}
-# variable can not have variable inside
-# local values can have variables inside
-# The most efficient way
-resource "aws_sqs_queue" "main" {
-    name = replace(local.name, "resource", "main-sqs") # referencing to "${var.provider}-${var.env}-${var.app}-main-sqs-${var.index}"
-    tags = local.common_tags
-}
-resource "aws_sqs_queue" "frontend" {
-    name = replace(local.name, "resource", "frontend-sqs") # referencing to "${var.provider}-${var.env}-${var.app}-frontend-sqs-${var.index}"
-    tags = local.common_tags
-}
-resource "aws_sqs_queue" "backend" {
-    name = replace(local.name, "resource", "backend-sqs") # referencing to "${var.provider}-${var.env}-${var.app}-backend-sqs-${var.index}"
-    tags = local.common_tags
+resource "aws_db_instance" "main" {
+  allocated_storage    = 10
+  identifier = replace(local.name, "resource", "rds") # this is the name of the instance created for RDS
+  db_name              = "mydb"         # this is database name inside RDS instance
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  username             = "admin"  # admin user
+  password             = random_password.db_password.result
+  skip_final_snapshot  = var.env != "prod" ? true : false # "prod" == "prod" ? false: true this means that final snapshot will be created, "fasle" means will create the snapshot
+  final_snapshot_identifier = var.env != "prod" ? null : "${var.env}-db-final-snapshot" # if the enviroment is prod the snapshot identifies is needed, otherwise not
 }
 
-# Another way - hard and crowded
-#resource "aws_sqs_queue" "main" {
-    #name = "${var.provider}-${var.env}-${var.app}-main-sqs-${var.index}"
-    #tags = merge(
-        #var.tags,
-        #{Name = "${var.provider}-${var.env}-${var.app}-main-sqs-${var.index}"}
- #   )
-#}
-#resource "aws_sqs_queue" "main" {
-    #name = "${var.provider}-${var.env}-${var.app}-frontend-sqs-${var.index}"
-    #tags = merge(
-        #var.tags,
-        #{Name = "${var.provider}-${var.env}-${var.app}-frontend-sqs-${var.index}"}
-    #)
-#}
-#resource "aws_sqs_queue" "main" {
-    #name = "${var.provider}-${var.env}-${var.app}-backend-sqs-${var.index}"
-    #tags = merge(
-        #var.tags,
-        #{Name = "${var.provider}-${var.env}-${var.app}-backend-sqs-${var.index}"}
-    #)
+#if enviroment is prod, set to false
+#else set to true
+#prod == prod ? skip_final_snapshot  = false : skip_final_snapshot  = true
+
+#resource "random_password" "db_password" {
+ #   length = 20
+ #   special = true
+ #   override_special = "%@"
 #}
 
-# merge is designed to merge multiple maps into a single map
-
-# The below is in the hard - not efficient way
-#resource "aws_sqs_queue" "main" {
-    #name = "${var.provider}-${var.env}-${var.app}-main-sqs-${var.index}"
-    #tags = {
-        #Enviroment = var.env
-        #Name = "${var.provider}-${var.env}-${var.app}-main-sqs-${var.index}"
-        #Team =  var.team
-        #App = var.app
-        #Managed_by = "Terraform"
-        #Owner = "Lidiya"
-        #Department = "BBC"
-    #} 
-#}
-#resource "aws_sqs_queue" "frontend" {
-    #name = "${var.provider}-${var.env}-${var.app}-frontend-sqs-${var.index}"
-    #tags = {
-       # Enviroment = var.env
-        #Name = "${var.provider}-${var.env}-${var.app}-frontend-sqs-${var.index}"
-        #Team =  var.team
-        #App = var.app
-        #Managed_by = "Terraform"
-        #Owner = "Lidiya"
-        #Department = "BBC"
-    #}
-#}
-#resource "aws_sqs_queue" "backend" {
-    #name = "${var.provider}-${var.env}-${var.app}-backend-sqs-${var.index}"
-    #tags = {
-        #Enviroment = var.env
-        #Name = "${var.provider}-${var.env}-${var.app}-backend-sqs-${var.index}"
-        #Team =  var.team
-        #App = var.app
-        #Managed_by = "Terraform"
-        #Owner = "Lidiya"
-        #Department = "BBC"
-    #} 
-#}
-
-#variable "provider"
-#variable "team"
-#variable "env"
-#variable "app"{
-#variable "index"
-  
-
-# Naming and tagging standarts
-
-// Naming Standard
-# teams = cloud, devops, qa, security
-#apps = elk, bean, lack, jerry, tom, pop
-# cloud providers = aws, azure, google
-# enviroment = dev, qa, stag, prod
-# providers-team-env-app-loadbalancer
-# aws-cloud-dev-jerry-loadbalancer#
-
- // Tagging Standard -  Common Tags:
- # Name = aws-cloud-dev-jerry-loadbalancer-0 # Name of the resource
- # Enviroment = Dev 
- # Team = cloud
- # App = Jerry 
- # Managed_by = "Terraform, CloudFomation, Python, Manual"
- # Owner = Lidiya
+#Equality Operators
+# Equal a==a: means a equals a
+# Doe snot Equal != , a != b : a doesn't equal b
+#Conditional Expression
