4b228582448d4a3147e5aa3fbce388a3ef089bf1
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-23 03:00:16 +0000

    updated

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -1,17 +1,19 @@
-resource "aws_security_group" "main" {   
-  name        = "${var.env}-sg"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port        = 22 # a single value
-    to_port          = 22
-    protocol         = "tcp"  # string
-    cidr_blocks      = ["68.129.177.23/32"] # list of strings
-  }
-   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1" # string # All ports and protocols
-    cidr_blocks      = ["0.0.0.0/0"] 
-  }
+resource "aws_security_group" "main" {
+    name        = "${var.env}-sg"
+    description = "Allow ports for inbound and outbound traffic of the server"
+    
+    ingress {
+        description      = "opening port for SSH connection inbound" 
+        from_port        = 22
+        to_port          = 22
+        protocol         = "tcp"
+        cidr_blocks      = ["68.129.177.23/32"]
+        }
+    egress {
+        description      = "Opening all ports for outbound traffic outbound" 
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }
 }
