55db32f73b36cd18644bfaa645b8ffe8d82edd8c
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-25 17:28:52 +0000

    adding session 10

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,1 +1,7 @@
 # Topic: calling a child module remotely 
+module "ec2" {
+    source = https://github.com/lkukhta12/terraform-session-january/tree/main/modules/modules/ec2
+    env = "dev"
+    ami = "ami-0aa7d40eeae50c9a9"
+    instance_type = "t2.micro"
+}
