45d9627b54f1c5bbd90e937645380f06956d4737
lkukhta12 <kukhtalidiya@gmail.com>
2023-02-25 18:48:24 +0000

    added version

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,10 +1,12 @@
 # Topic: calling a child module remotely 
 module "ec2" {
-    source = "github.com/lkukhta12/terraform-session-january/modules/modules/ec2"
+    source = "github.com/lkukhta12/terraform-session-january//modules/modules/ec2?ref=v1.0.1"
     env = "dev"
     ami = "ami-0aa7d40eeae50c9a9"
     instance_type = "t2.micro"
 }
+
+
 #Tree of dependencies 
 #github.com = Domain name 
 #Akumo = organization
